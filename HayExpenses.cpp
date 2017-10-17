bfc4cdb052d0f62588122914d0709f01166f6c7e
Dougy Ouyang <ouyangdougy@163.com>
2017-10-16 17:05:58 -0700

    Create HayExpenses.cpp

---
+++ HayExpenses.cpp
@@ -0,0 +1,55 @@
+/*
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, q, hay[1024];
+    int querys, querye, sum[505];
+    int i, j;
+    
+    cin >> n >> q;
+    for(i=0;i<n;i++)
+        cin >> hay[i];
+    for(i=0;i<q;i++)
+    {
+        cin >> querys >> querye;
+        sum[i]=0;
+        for(j=querys-1;j<querye;j++)
+            sum[i]+=hay[j];
+    }
+    
+    for(j=0;j<i;j++)
+        cout << sum[j] << endl;
+    
+    return 0;
+}
